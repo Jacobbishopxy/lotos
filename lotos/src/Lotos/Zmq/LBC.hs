933f2cbb5bfd2f8e2aad52d2a6c820f585e7186c
jacob <jacobbishopxy@gmail.com>
2025-04-07 22:50:32 +0800

    📝 update `lb_sys.drawio`

---
+++ lotos/src/Lotos/Zmq/LBC.hs
@@ -0,0 +1,9 @@
+-- file: LBC.hs
+-- author: Jacob Xie
+-- date: 2025/04/07 21:51:04 Monday
+-- brief:
+
+module Lotos.Zmq.LBC
+  (
+  )
+where
--- lotos/src/Lotos/Zmq/LBW.hs
+++ lotos/src/Lotos/Zmq/LBW.hs
@@ -2,6 +2,10 @@
 -- author: Jacob Xie
 -- date: 2025/04/06 20:21:31 Sunday
 -- brief:
+--
+-- 1. Acceptor (Dealer) asynchronously receives tasks from the frontend socket.
+-- 2. Sender (Pair -> Dealer) cross thread sends tasks to the backend socket.
+-- 3. Publisher (Pair -> Pub) cross thread sends logging messages to the logging socket.
 
 module Lotos.Zmq.LBW
   (
